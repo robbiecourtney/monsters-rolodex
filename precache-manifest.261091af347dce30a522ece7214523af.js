a005e119012262eddd02eb36cbf13dd11a5d997e
Robbie Courtney <robbiecourtney@gmail.com>
2022-01-05 12:59:12 -0600

    Updates

---
+++ precache-manifest.261091af347dce30a522ece7214523af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29561cf6ee7b893d053278103b6e995c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "65508313974fd65091c1",
+    "url": "/monsters-rolodex/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65508313974fd65091c1",
+    "url": "/monsters-rolodex/static/js/main.163366ea.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
